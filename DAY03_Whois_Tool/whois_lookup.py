4754e98d9c286c45cf51bd4c3be10ccdeb335020
Abhinav Jassi <97092496+abj21@users.noreply.github.com>
2026-06-06 15:22:07 +0530

    Update whois_lookup.py

--- DAY03_Whois_Tool/whois_lookup.py
+++ DAY03_Whois_Tool/whois_lookup.py
@@ -1,1 +1,19 @@
+import whois
 
+domain = input("Enter domain name: ")
+
+try:
+    info = whois.whois(domain)
+
+    print("\n===== WHOIS REPORT =====")
+    print("Domain Name      :", info.domain_name)
+    print("Registrar        :", info.registrar)
+    print("Creation Date    :", info.creation_date)
+    print("Expiration Date  :", info.expiration_date)
+    print("Updated Date     :", info.updated_date)
+    print("Organization     :", info.org)
+    print("Country          :", info.country)
+    print("Name Servers     :", info.name_servers)
+
+except Exception as e:
+    print("Error:", e)
